811baac135fe89780b122d940af80c44f5f586a3
Tabitha-Twitchit <Tabitha@Tabithanikolai.com>
2026-08-09 20:37:00 -0700

    Enforced Single Instancing

--- lafangitime/src-tauri/src/lib.rs
+++ lafangitime/src-tauri/src/lib.rs
@@ -41,6 +41,12 @@ fn find_node() -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            // focus existing window
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, None))
         .invoke_handler(tauri::generate_handler![greet])
